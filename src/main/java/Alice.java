ab2f4b5f700cac650534d66f04c8770505b97836
Edison-choo <72715139+Edison-choo@users.noreply.github.com>
2026-08-18 15:41:13 +0800

    Change the name of the chatbot

---
+++ src/main/java/Alice.java
@@ -0,0 +1,12 @@
+public class Alice {
+    public static void main(String[] args) {
+        String banner = """
+                ██████  ██     ████  █████  █████
+                ██  ██  ██      ██   ██     ██
+                ██████  ██      ██   ██     ████
+                ██  ██  ██      ██   ██     ██
+                ██  ██  █████  ████  █████  █████
+                """;
+        System.out.println(banner);
+    }
+}
