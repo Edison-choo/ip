370cde8d65e726d0d9e56d97c8488384030976a7
Edison-choo <72715139+Edison-choo@users.noreply.github.com>
2026-08-18 17:00:28 +0800

    Store input and display list

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,28 +1,49 @@
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Alice {
-    public static void main(String[] args) {
-        printGreetings();
+    // Instance variable
+    private ArrayList<String> tasks;
 
+    public Alice() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        Alice alice = new Alice();
+        alice.run();
+    }
+
+    public void run() {
+        // Setup
         Scanner scanner = new Scanner(System.in);
 
+        printGreetings();
+
+        mainLoop:
         while (true) {
             // Read user input
             String input = scanner.nextLine();
 
-            // Check if user wants to quit
-            if (Objects.equals(input, "bye")) {
-                quitMessage();
-                break;
+            switch (input) {
+                case "bye":
+                    quitMessage();
+                    break mainLoop;
+                case "list":
+                    listTasks();
+                    break;
+                default:
+                    addTask(input);
+                    break;
             }
 
             // Echo user input
-            echoInput(input);
+            // echoInput(input);
         }
     }
 
-    public static void printGreetings() {
+    public void printGreetings() {
         String greetings = """
                 ----------------------------------------------------------
                 ██████  ██     ████  █████  █████
@@ -37,14 +58,31 @@ public class Alice {
         System.out.println(greetings);
     }
 
-    public static void echoInput(String input) {
+    public void echoInput(String input) {
         System.out.println(input);
         System.out.println("----------------------------------------------------------");
     }
 
-    public static void quitMessage() {
+    public void quitMessage() {
         System.out.println("----------------------------------------------------------");
         System.out.println("Bye Bye. Can't wait to talk to you again!");
         System.out.println("----------------------------------------------------------");
+    }
+
+    public void addTask(String task) {
+        this.tasks.add(task);
+        System.out.printf("added: %s%n", task);
+        System.out.println("----------------------------------------------------------");
+    }
+
+    public void listTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks in your list yet!");
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.printf("%d. %s\n", i+1, tasks.get(i));
+            }
+            System.out.println("----------------------------------------------------------");
+        }
     }
 }
