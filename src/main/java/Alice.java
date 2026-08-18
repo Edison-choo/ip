07c294d25b1e774cf188208ecf278872dbcd0e62
Edison-choo <72715139+Edison-choo@users.noreply.github.com>
2026-08-18 16:34:46 +0800

    Echo input and quit function

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,6 +1,29 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Alice {
     public static void main(String[] args) {
-        String banner = """
+        printGreetings();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            // Read user input
+            String input = scanner.nextLine();
+
+            // Check if user wants to quit
+            if (Objects.equals(input, "bye")) {
+                quitMessage();
+                break;
+            }
+
+            // Echo user input
+            echoInput(input);
+        }
+    }
+
+    public static void printGreetings() {
+        String greetings = """
                 ----------------------------------------------------------
                 ██████  ██     ████  █████  █████
                 ██  ██  ██      ██   ██     ██
@@ -10,11 +33,18 @@ public class Alice {
                 
                 Hello! Alice is here to chat!
                 What do you want to discuss with me?
-                ----------------------------------------------------------
-                Bye Bye. Can't wait to talk to you again!
-                ----------------------------------------------------------
-                
-                """;
-        System.out.println(banner);
+                ----------------------------------------------------------""";
+        System.out.println(greetings);
+    }
+
+    public static void echoInput(String input) {
+        System.out.println(input);
+        System.out.println("----------------------------------------------------------");
+    }
+
+    public static void quitMessage() {
+        System.out.println("----------------------------------------------------------");
+        System.out.println("Bye Bye. Can't wait to talk to you again!");
+        System.out.println("----------------------------------------------------------");
     }
 }
