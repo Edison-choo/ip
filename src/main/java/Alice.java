3ac7179a8ef6374889f023c50f7c3bce373244c7
Edison-choo <72715139+Edison-choo@users.noreply.github.com>
2026-08-18 15:47:53 +0800

    Add greetings and exit message

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,11 +1,19 @@
 public class Alice {
     public static void main(String[] args) {
         String banner = """
+                ----------------------------------------------------------
                 ██████  ██     ████  █████  █████
                 ██  ██  ██      ██   ██     ██
                 ██████  ██      ██   ██     ████
                 ██  ██  ██      ██   ██     ██
                 ██  ██  █████  ████  █████  █████
+                
+                Hello! Alice is here to chat!
+                What do you want to discuss with me?
+                ----------------------------------------------------------
+                Bye Bye. Can't wait to talk to you again!
+                ----------------------------------------------------------
+                
                 """;
         System.out.println(banner);
     }
