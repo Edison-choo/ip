0429e0a531b5933002c34e45cf2a9563d02cdade
Edison-choo <72715139+Edison-choo@users.noreply.github.com>
2026-08-24 23:39:18 +0800

    Create file writing and reading feature

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,10 @@ public abstract class Task {
         this.isDone = false;
     }
 
+    public String getDescription() {
+        return this.description;
+    }
+
     public String getStatusIcon() {
         return (isDone ? "x" : " ");
     }
