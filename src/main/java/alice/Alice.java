1aedb7b08654c0c48590ad73cf8c1a275e2a7b30
Edison-choo <72715139+Edison-choo@users.noreply.github.com>
2026-08-25 20:23:57 +0800

    Fix the code to follow coding standard

--- src/main/java/alice/Alice.java
+++ src/main/java/alice/Alice.java
@@ -81,6 +81,7 @@ public class Alice {
                     break;
                 case DELETE:
                     deleteTask(input);
+                    break;
                 case VIEW:
                     viewDate(input);
                     break;
